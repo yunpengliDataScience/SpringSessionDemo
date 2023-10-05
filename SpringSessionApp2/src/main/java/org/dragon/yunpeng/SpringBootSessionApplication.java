8014b6197b045d5af2c6309be7dce653b5cba0ab
Yunpeng Li <yli4@umbc.edu>
2023-10-05 18:51:31 -0400

    Added third application that is based on classic Spring XML configuration

--- SpringSessionApp2/src/main/java/org/dragon/yunpeng/SpringBootSessionApplication.java
+++ SpringSessionApp2/src/main/java/org/dragon/yunpeng/SpringBootSessionApplication.java
@@ -2,9 +2,10 @@ package org.dragon.yunpeng;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootSessionApplication {
+public class SpringBootSessionApplication extends SpringBootServletInitializer{  //Need to extends SpringBootServletInitializer to be able to deployed in Tomcat
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootSessionApplication.class, args);
